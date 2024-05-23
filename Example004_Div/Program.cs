908cd03ea865938fad3beda21fafe222799853be
Alexandra <aaleexaandra@email.cz>
2024-05-23 12:39:05 +0200

    example004 div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу в которой пользователь вводит два целых числа и получает частное введенных чисел.
+//Это работает с деление целых чисел,поэтому в ответе будет только целое число.
+// int numberA = 10;
+// int numberB = 5;
+// System.Console.WriteLine(numberA / numberB);
+
+// Для того чтобы в ответе получить дробные числа:
+
+double numberA = 12;
+double numberB = 5;
+System.Console.WriteLine(numberA / numberB);
