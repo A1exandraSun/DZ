971b5bd0ac50a8e6547ddb49092465e931fa11d1
Alexandra <aaleexaandra@email.cz>
2024-05-25 09:16:03 +0200

    random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -3,7 +3,15 @@
 // int numberB = 5;
 // System.Console.WriteLine(numberA + numberB);
 
-int numberA = 31;
-int numberB = 5;
+// int numberA = 31;
+// int numberB = 5;
+// int result = numberA + numberB;
+// System.Console.WriteLine(result);
+
+
+int numberA = new Random().Next(1, 10); // 0 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 System.Console.WriteLine(result);
